98ddcd98a1e49616b3e92fa693957f729454f54f
Laura Lindzey <lindzey@gmail.com>
2020-06-14 10:08:57 -0400

    minor cleanup on day13

--- 2016/day13/src/main.rs
+++ 2016/day13/src/main.rs
@@ -10,7 +10,7 @@ mod tests {
 
     #[test]
     fn test_is_wall() {
-        // The example inputs have 10 as teh designer's favorite number
+        // The example inputs have 10 as the designer's favorite number
         let num = 10;
         assert!(!is_wall(&Coord { x: 0, y: 0 }, num));
         assert!(!is_wall(&Coord { x: 2, y: 0 }, num));
@@ -58,14 +58,24 @@ struct Coord {
     y: u32,
 }
 
+/**
+ * In order to determine whether a cell is a wall:
+ * - Find x*x + 3*x + 2*x*y + y + y*y.
+ * - Add the office designer's favorite number (your puzzle input).
+ * - Find the binary representation of that sum; count the number of bits that are 1.
+ *   - If the number of bits that are 1 is even, it's an open space.
+ *   - If the number of bits that are 1 is odd, it's a wall.
+ */
 fn is_wall(pt: &Coord, offset: u32) -> bool {
-    let foo = pt.x * pt.x + 3 * pt.x + 2 * pt.x * pt.y + pt.y + pt.y * pt.y + offset;
-    let num_ones = foo.count_ones();
+    let val = pt.x * pt.x + 3 * pt.x + 2 * pt.x * pt.y + pt.y + pt.y * pt.y + offset;
+    let num_ones = val.count_ones();
     num_ones % 2 == 1
 }
 
 fn get_neighbors(pt: &Coord) -> HashSet<Coord> {
     let mut neighbors: HashSet<Coord> = HashSet::new();
+
+    // There is no upper bound to valid coordinates
     neighbors.insert(Coord {
         x: pt.x,
         y: pt.y + 1,
@@ -74,6 +84,8 @@ fn get_neighbors(pt: &Coord) -> HashSet<Coord> {
         x: pt.x + 1,
         y: pt.y,
     });
+
+    // The office does not extend into negative coordinates
     if pt.x > 0 {
         neighbors.insert(Coord {
             x: pt.x - 1,
@@ -86,6 +98,7 @@ fn get_neighbors(pt: &Coord) -> HashSet<Coord> {
             y: pt.y - 1,
         });
     }
+
     neighbors
 }
 
@@ -104,20 +117,22 @@ impl PartialOrd for State {
 
 impl Ord for State {
     fn cmp(&self, other: &State) -> Ordering {
+        // flip the ordering so lower costs have priority
         other.cost.cmp(&self.cost)
     }
 }
 
+// Run Dijkstra's algorithm (closely following the example in the
+// BinaryHeap docs), and return the length of the path.
 // TODO: This feels like it could be made cleaner with references.
 //  (currently has two copies of each point)
-// Run Dijkstra's algorithm (closely following the example in the BinaryHeap docs)
 fn shortest_path(offset: u32, start: Coord, end: Coord) -> usize {
     let mut dists: HashMap<Coord, usize> = HashMap::new();
     dists.insert(start.clone(), 0);
     let mut open = BinaryHeap::new();
     open.push(State { cost: 0, pt: start });
     while let Some(State { cost, pt }) = open.pop() {
-        println!("Exploring pt: {}, {} with cost {}", pt.x, pt.y, cost);
+        // println!("Exploring pt: {}, {} with cost {}", pt.x, pt.y, cost);
         if pt == end {
             return cost;
         }
@@ -138,6 +153,7 @@ fn shortest_path(offset: u32, start: Coord, end: Coord) -> usize {
     panic!("Have run out of states to explore but haven't found goal");
 }
 
+// Determine how many cells are reachable from the start in a given number of steps
 fn flood_fill(offset: u32, start: Coord, steps: usize) -> usize {
     // Tracks the current shortest distance to any visited point
     let mut dists: HashMap<Coord, usize> = HashMap::new();
@@ -147,7 +163,7 @@ fn flood_fill(offset: u32, start: Coord, steps: usize) -> usize {
     open.push(State { cost: 0, pt: start });
 
     while let Some(State { cost, pt }) = open.pop() {
-        println!("Exploring pt: {}, {} with cost {}", pt.x, pt.y, cost);
+        // println!("Exploring pt: {}, {} with cost {}", pt.x, pt.y, cost);
         if cost > steps {
             break;
         }
@@ -155,7 +171,7 @@ fn flood_fill(offset: u32, start: Coord, steps: usize) -> usize {
             if !is_wall(&neighbor, offset) {
                 let curr_cost = *dists.get(&neighbor).unwrap_or(&usize::MAX);
                 if cost + 1 < curr_cost {
-                    println!("...adding neighbor {}, {} at cost {}", neighbor.x, neighbor.y, cost + 1);
+                    // println!("...adding neighbor {}, {} at cost {}", neighbor.x, neighbor.y, cost + 1);
                     open.push(State {
                         cost: cost + 1,
                         pt: neighbor.clone(),
@@ -173,19 +189,24 @@ fn flood_fill(offset: u32, start: Coord, steps: usize) -> usize {
     reachable.len()
 }
 
+/**
+ * Part 1 asks for the minimum number of steps required to reach a given point.
+ */
 fn part1() -> usize {
-    let num = 1364;
+    let offset = 1364;
     let start_point = Coord { x: 1, y: 1 };
     let end_point = Coord { x: 31, y: 39 };
-    let path_length = shortest_path(num, start_point, end_point);
-    path_length
+    shortest_path(offset, start_point, end_point)
 }
 
+/**
+ * Part 2 asks for how many locations can be reached in a given number of steps.
+ */
 fn part2() -> usize {
     let num = 1364;
     let start_point = Coord { x: 1, y: 1 };
-    let num_reachable = flood_fill(num, start_point, 50);
-    num_reachable
+    let num_steps = 50;
+    flood_fill(num, start_point, num_steps)
 }
 
 fn main() {
